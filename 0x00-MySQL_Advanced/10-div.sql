882ee643a34c18741cbbb060d3eb502482891bb6
salihabrik <briksaliha@gmail.com>
2024-01-18 21:36:36 +0100

    Safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- Create a function SafeDiv
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
