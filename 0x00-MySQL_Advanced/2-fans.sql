d1a215b6a9520a731fe22c6ac56fd346df1e15f5
salihabrik <briksaliha@gmail.com>
2024-01-18 21:16:28 +0100

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,9 @@
+-- Import the metal_bands table dump
+-- (Assuming the metal_bands.sql file contains the table dump)
+-- If you need to load the data first, use the following:
+-- mysql -uroot -p holberton < metal_bands.sql
+
+-- Query to rank country origins of bands by number of fans
 SELECT origin, COUNT(fan_id) AS nb_fans
 FROM metal_bands
 GROUP BY origin
