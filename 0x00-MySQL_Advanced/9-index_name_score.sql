2c067dcf337d30ec4aa175ca26814b7f9797d5cb
salihabrik <briksaliha@gmail.com>
2024-01-18 21:42:55 +0100

    Optimize search and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,3 @@
--- Create an index on the first letter of the name and the score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+-- Creates an index idx_name_first_score on the table names and
+-- the first letter of name and the score.
+CREATE INDEX idx_name_first_score ON names(name(1), score);
