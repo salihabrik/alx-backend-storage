18a6e8bc6de53110e86148c52dc05a3d196b3795
salihabrik <briksaliha@gmail.com>
2024-01-23 20:45:57 +0100

    Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''Task 15's module.
+'''
+
+
+
+from pymongo import MongoClient
+
+def print_nginx_request_logs(collection):
+    print(f"{collection.count_documents({})} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{count} status check")
+
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+def run():
+    '''
+    stats about Nginx logs stored in MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+
+if __name__ == '__main__':
+    run()
