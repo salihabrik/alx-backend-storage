10de31f9784e30a0d9d3348673f0873f47a04233
salihabrik <briksaliha@gmail.com>
2024-01-24 23:55:06 +0100

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,32 +8,39 @@ from functools import wraps
 
 
 class Cache:
-    def __init__(self):
+    '''Represents an object for storing data in a Redis data storage.
+    '''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance.
+        '''
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
-    def store(self, data) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-    def get(self, key: str, fn: Callable = None):
-        value = self._redis.get(key)
-        return fn(value) if fn else value
-
-    def get_str(self, key: str):
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str):
-        return self.get(key, fn=int)
-    
-    def count_calls(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            key = func.__qualname__
-            count_key = f"count:{key}"
-            self._redis.incr(count_key)
-            return func(self, *args, **kwargs)
-        return wrapper
-
+    @call_history
     @count_calls
-    def store(self, data) -> str:  
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an integer value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: int(x)) 
