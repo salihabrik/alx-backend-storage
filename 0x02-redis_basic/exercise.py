37839ddf80761ceb69cbe11c50820496cc04c633
salihabrik <briksaliha@gmail.com>
2024-01-24 23:44:05 +0100

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''A module for using the Redis NoSQL data storage.
+'''
+import redis
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
